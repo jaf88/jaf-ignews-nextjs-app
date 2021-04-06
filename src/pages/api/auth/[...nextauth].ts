005efffe9be7d4804a3b400c228862eec92d0f54
jaf88 <jaf.alves8@gmail.com>
2021-04-06 13:43:14 -0300

    feat:Integrations on FaunaDb and Stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,12 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { signIn } from 'next-auth/client'
+
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
+
   // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
@@ -9,6 +14,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-    // ...add more providers here
   ],
+  
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
