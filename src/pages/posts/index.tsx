7479b4b2fb72bc0b8f7027710845795a54d74d5d
jaf88 <jaf.alves8@gmail.com>
2021-04-08 16:07:30 -0300

    feat: CMS module

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -6,6 +6,8 @@ import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
+import React from 'react';
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -28,11 +30,13 @@ export default function Posts({ posts}: PostsProps ) {
       <main className={styles.container}>
         <div className={styles.posts}>
           { posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           )) }
         </div>
       </main>
